3fe5263573d2c75944b967f16a38e5352d48953b
Matt McGrattan <matt.mcgrattan@digirati.com>
2017-05-12 17:09:37 +0100

    Reformatting, moved the print statement out of the parse function.

--- r_template.py
+++ r_template.py
@@ -3,6 +3,11 @@ import json
 import requests
 import collections
 
+"""
+Grab Omeka resource templates and iterate through to generate a single
+readable JSON representation.
+"""
+
 
 def get_item(item_uri):
     r = requests.get(item_uri)
@@ -26,22 +31,29 @@ def item_processor(class_uri):
     else:
         return None
 
+
 def parse_template(t):
     template = item_processor(t['o:resource_class']['@id'])
-    template['label'] =  t['o:label']
+    template['label'] = t['o:label']
     if template:
         template['fields'] = []
         for item in t['o:resource_template_property']:
             i = item['o:property']['@id']
             c = item_processor(i)
             template['fields'].append(c)
-    print(json.dumps(template, sort_keys=False, indent=4))
+        return template
+    else:
+        return None
+
 
 def main():
-    for r in range(4,8):
+    for r in range(4, 9):
         t = get_item('http://omeka.dlcs-ida.org/api/resource_templates/' + str(r))
         if t:
-            parse_template(t)
+            temp = parse_template(t)
+            if temp:
+                print(json.dumps(temp, sort_keys=False, indent=4))
+
 
 if __name__ == '__main__':
     main()
